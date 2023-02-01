7c2b57a069b6f611ae5f59c91d9f641aa83859a1
kavindu <wmkavinduudhara@gmail.com>
2023-02-02 00:08:41 +0530

    functions for servo motor
    created functions called:
    servo_motor_forward()
    servo_motor_left() and
    servo_motor_right()
    
    to rotate servo motor while turning the car chassic

--- robot_car_1/robot_car_1.ino
+++ robot_car_1/robot_car_1.ino
@@ -27,6 +27,8 @@ void setup() {
   analogWrite(EnA, 150);
   analogWrite(EnB, 150);
 
+  servo.attach(A0);
+
   Serial.begin(9600);
 }
 
@@ -35,28 +37,23 @@ void loop() {
   Serial.println(connection);
 
   if (connection == '1'){
+    servo_motor_forward();
     go_forward();
-    delay(500);
-    stop();
-    delay(500);
+    after_action();
   }
   if (connection == '2'){
     go_backward();
-    delay(500);
-    stop();
-    delay(500);
+    after_action();
   }
   if (connection == '3'){
+    servo_motor_right();
     turn_right();
-    delay(500);
-    stop();
-    delay(500);
+    after_action();
   }
   if (connection == '4'){
+    servo_motor_left();
     turn_left();
-    delay(500);
-    stop();
-    delay(500);
+    after_action();
   }
   if (connection == '5'){
     stop();
@@ -98,4 +95,39 @@ void turn_right(){
   digitalWrite(In2, LOW);
   digitalWrite(In3, HIGH);
   digitalWrite(In4, LOW);
+}
+
+void after_action(){
+    delay(500);
+    stop();
+    delay(500);
+}
+
+void servo_motor_forward(){
+  servo.write(30);
+  delay(500);
+  servo.write(90);
+  delay(500);
+  servo.write(150);
+  delay(500);
+  servo.write(90);
+  delay(500);
+}
+
+void servo_motor_left(){
+  servo.write(150);
+  delay(500);
+  servo.write(90);
+  delay(500);
+  servo.write(150);
+  delay(500);
+}
+
+void servo_motor_right(){
+  servo.write(30);
+  delay(500);
+  servo.write(90);
+  delay(500);
+  servo.write(30);
+  delay(500);
 }
