6d90cd314599890e476597fa9efeba74755a24ea
kavindu <wmkavinduudhara@gmail.com>
2023-02-02 11:10:10 +0530

    document the file properly

--- robot_car_1/robot_car_1.ino
+++ robot_car_1/robot_car_1.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-
+//servo motor
 Servo servo = Servo();
 
 int EnA = 5;
@@ -13,22 +13,23 @@ int In4 = 11;
 char connection;
 
 void setup() {
-  //turn moter A
+  //servo.attach(A0); //servo motor pin
+
+  //L298N-2A Motor Driver Module | motorA - pins
   pinMode(EnA, OUTPUT);
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
 
-  //turn moter B
+  //L298N-2A Motor Driver Module | motorB - pins
   pinMode(EnB, OUTPUT);
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
 
-  //lowest - 100 highest - 200
+  //define the speed of the motors | lowest - 100 | highest - 200
   analogWrite(EnA, 150);
   analogWrite(EnB, 150);
 
-  servo.attach(A0);
-
+  //for bluetooth signals
   Serial.begin(9600);
 }
 
@@ -62,6 +63,7 @@ void loop() {
 }
 
 void go_forward(){
+  //go forward | In1 -> HIGH | In2 -> LOW || In3 -> LOW | In4 -> HIGH
   digitalWrite(In1, HIGH);
   digitalWrite(In2, LOW);
   digitalWrite(In3, LOW);
@@ -69,7 +71,7 @@ void go_forward(){
 }
 
 void go_backward(){
-  // In1 -> HIGH & In2 -> LOW ---- backward
+  //go backward | In1 -> HIGH | In2 -> LOW || In3 -> LOW | In4 -> HIG
   digitalWrite(In1, LOW);
   digitalWrite(In2, HIGH);
   digitalWrite(In3, HIGH);
