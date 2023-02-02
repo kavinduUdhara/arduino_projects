aee968ed565b53542bed7b1c93fdcc17110e4636
kavindu <wmkavinduudhara@gmail.com>
2023-02-02 13:29:41 +0530

    added  <Ultrasonic.h> module and imp. docu.
    added some features with ultrasonic module:
        int distance = ultrasonicSensor.distanceRead(); // Measure the distance using the ultrasonic sensor
        // Check if the distance measured is not 0 and greater than 10
        if (distance != 0 and distance > 10){
          go_forward();
          after_action();
        }

--- robot_car_1/robot_car_1.ino
+++ robot_car_1/robot_car_1.ino
@@ -2,20 +2,19 @@
   Project: Arduino Controlled Car with Servo Motor, Ultrasonic Sensor and bluetooth moduler
   Purpose: This code provides to control an arduino-powered car with a servo motor and ultrasonic sensor. The car can receive commands through Bluetooth to move forward, backward, turn left, turn right and stop.
   Function: The servo motor acts as the steering mechanism, while the ultrasonic sensor is used for obstacle detection.
+  Auther: (c) Kavindu Udhara | all rights reserved
 */
 
 #include <Servo.h> // Include Servo library
 #include <Ultrasonic.h> // Include Ultrasonic library
 /*The Servo.h library is used to control a servo motor in the project. It provides functions for setting the servo motor's position, speed, and other properties.
-
 The Ultrasonic.h library is used to measure the distance between the ultrasonic sensor and an object in front of it. It provides functions to trigger the ultrasonic sensor and receive the time required for the echo signal to return. This time can then be used to calculate the distance to the object.
-
 Both of these libraries simplify the implementation of these components in the project, making it easier to control the servo motor and measure distance with the ultrasonic sensor.*/
 
 // Servo Motor
 Servo servo = Servo(); // Create an object for the servo motor
 // Ultrasonic Sensor
-Ultrasonic u = Ultrasonic(A2, A1);  // ultrasonic sensor is connected to pins A2 (Trig Pin) and A1 (Echo Pin)
+Ultrasonic ultrasonicSensor = Ultrasonic(A2, A1);  // ultrasonic sensor is connected to pins A2 (Trig Pin) and A1 (Echo Pin)
 
 // L298N Motor Driver
 // Motor A
@@ -27,10 +26,10 @@ int In2 = 5; // Input 2 Pin for motor A
 int EnB = 11; // Enable Pin for motor B
 int In3 = 12; // Input 3 Pin for motor B
 int In4 = 13; // Input 4 Pin for motor B
-/*...use of the library disables analogWrite() (PWM) functionality on pins 9 and 10,... 
+/*Ref. to the error #1 on GitHub: ...use of the library disables analogWrite() (PWM) functionality on pins 9 and 10,... 
 ref: https://stackoverflow.com/questions/75318922/motor-driver-isnt-working-properly-while-servo-attacha0-line-present-in-the*/
 
-char connection;
+char connection; //for read bluetooth module connection signels
 
 void setup() {
   servo.attach(A0); // servo motor is connected to pin A0 
@@ -53,36 +52,53 @@ void setup() {
   Serial.begin(9600);  // Start serial communication at 9600 baud rate
 }
 
+// This is the main loop function where the program listens to the bluetooth connection signals 
 void loop() {
-  //read bluetooth connection signels
+  // Read the signal received from the bluetooth connection
   connection = Serial.read();
-  Serial.println(connection);
+  Serial.println(connection); // Print the received signal on the serial monitor
 
+  // Check if the received signal is '1'
   if (connection == '1'){
     servo_motor_forward();
-    go_forward();
-    after_action();
+    int distance = ultrasonicSensor.distanceRead(); // Measure the distance using the ultrasonic sensor
+    // Check if the distance measured is not 0 and greater than 10
+    if (distance != 0 and distance > 10){
+      go_forward();
+      after_action();
+    }
   }
+  // Check if the received signal is '2'
   if (connection == '2'){
     go_backward();
     after_action();
   }
+  // Check if the received signal is '3'
   if (connection == '3'){
     servo_motor_right();
-    turn_right();
-    after_action();
+    int distance = ultrasonicSensor.distanceRead();
+    if (distance != 0 and distance > 10){
+      turn_right();
+      after_action();
+    }
   }
+  // Check if the received signal is '4'
   if (connection == '4'){
     servo_motor_left();
-    turn_left();
-    after_action();
+    int distance = ultrasonicSensor.distanceRead();
+    if (distance != 0 and distance > 10){
+      turn_left();
+      after_action();
+    }
   }
+  // Check if the received signal is '5'
   if (connection == '5'){
     stop();
-    delay(500);
+    delay(500); // Wait for 500ms
   }
 }
 
+// This function moves the robot forward
 void go_forward(){
   //go forward | In1 -> HIGH | In2 -> LOW || In3 -> LOW | In4 -> HIGH
   digitalWrite(In1, HIGH);
@@ -91,6 +107,7 @@ void go_forward(){
   digitalWrite(In4, HIGH);
 }
 
+// This function moves the robot backward
 void go_backward(){
   //go backward | In1 -> LOW | In2 -> HIGH  || In3 -> HIGH | In4 -> LOW
   digitalWrite(In1, LOW);
@@ -99,6 +116,7 @@ void go_backward(){
   digitalWrite(In4, LOW);
 }
 
+// This function stops the robot
 void stop(){
   //stop | * -> LOW
   digitalWrite(In1, LOW);
@@ -107,6 +125,7 @@ void stop(){
   digitalWrite(In4, LOW);
 }
 
+// Function to turn the robot to the left
 void turn_left(){
   digitalWrite(In1, LOW);
   digitalWrite(In2, HIGH);
@@ -114,6 +133,7 @@ void turn_left(){
   digitalWrite(In4, HIGH);
 }
 
+// Function to turn the robot to the right
 void turn_right(){
   digitalWrite(In1, HIGH);
   digitalWrite(In2, LOW);
@@ -121,37 +141,41 @@ void turn_right(){
   digitalWrite(In4, LOW);
 }
 
+// Function to perform actions after turning the robot
 void after_action(){
-    delay(500);
-    stop();
-    delay(500);
+    delay(500); // Wait for 500ms
+    stop(); // Stop the robot
+    delay(500); // Wait for 500ms
 }
 
+// Function to rotate the servo motor to the forward direction
 void servo_motor_forward(){
-  servo.write(30);
+  servo.write(30); // Rotate the servo motor to 30 degrees
   delay(500);
-  servo.write(90);
+  servo.write(90); // Rotate the servo motor to 90 degrees
   delay(500);
-  servo.write(150);
+  servo.write(150); // Rotate the servo motor to 150 degrees
   delay(500);
-  servo.write(90);
+  servo.write(90); // Rotate the servo motor back to 90 degrees
   delay(500);
 }
 
+// Function to rotate the servo motor to the left direction
 void servo_motor_left(){
-  servo.write(150);
+  servo.write(150); // Rotate the servo motor to 150 degrees
   delay(500);
-  servo.write(90);
+  servo.write(90);  // Rotate the servo motor back to 90 degrees
   delay(500);
-  servo.write(150);
+  servo.write(150); // Rotate the servo motor to 150 degrees again
   delay(500);
 }
 
+// Function to rotate the servo motor to the right direction
 void servo_motor_right(){
-  servo.write(30);
+  servo.write(30); // Rotate the servo motor to 30 degrees
   delay(500);
-  servo.write(90);
+  servo.write(90); // Rotate the servo motor back to 90 degrees
   delay(500);
-  servo.write(30);
+  servo.write(30); // Rotate the servo motor to 30 degrees again
   delay(500);
 }
